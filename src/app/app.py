3501888911816c89b7a51b4caa8834b6aefcd6ff
tetn39 <tetn39@gmail.com>
2024-05-09 15:19:30 +0900

    Add Flask request module and update index.html template***
    ***Add login.html template and style.css file

--- src/app/app.py
+++ src/app/app.py
@@ -1,18 +1,21 @@
-#Flaskとrender_template（HTMLを表示させるための関数）をインポート
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
-#Flaskオブジェクトの生成
 app = Flask(__name__)
 
 
-#「/」へアクセスがあった場合に、"Hello World"の文字列を返す
 @app.route("/")
 def hello():
     return "Hello World"
 
 
-#「/index」へアクセスがあった場合に、「index.html」を返す
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    name = request.args.get("name")
 
+    num = [1,2,3,4,5,6,7,8,9,10]
+
+    return render_template("index.html", aaa=name, num=num)
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
