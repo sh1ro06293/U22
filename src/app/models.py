dd22c4873f14989e37ee6dca6ee6821efd0befc0
githubsh1ro06293 <githubshiro.06293@gmail.com>
2024-05-23 12:31:57 +0900

    feat: Add StationTable model to handle station data
    This commit adds a new model called StationTable to handle station data. The model includes fields for id, Name, and Password.

--- src/app/models.py
+++ src/app/models.py
@@ -8,3 +8,9 @@ class UserTable(db.Model, UserMixin):
     Email = db.Column(db.String(100), nullable=False, unique=True)
     Number = db.Column(db.String(15), nullable=False)
     Password = db.Column(db.String(100), nullable=False)
+
+class StationTable(db.Model, UserMixin):
+    __tablename__ = 'StationTable'
+    id = db.Column(db.String(20), primary_key=True)
+    Name = db.Column(db.String(50), nullable=False)
+    Password = db.Column(db.String(100), nullable=False)
